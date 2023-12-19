dd11dce0f72a647eac72916ec6203e64ff4aa237
haidarabdillah <113880800+haidarabdillah@users.noreply.github.com>
2023-12-19 18:21:04 +0700

    add mongodb repo

--- prestart.sh
+++ prestart.sh
@@ -5,11 +5,17 @@ DB_NAME="explorerdb"
 DB_USER="userrpc"
 DB_PASS="changeMeplease"
 
+# Import MongoDB GPG Key
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+
+# Add MongoDB Repository
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
 # Update package list
 sudo apt update
 
 # Install MongoDB
-sudo apt install -y mongodb
+sudo apt install -y mongodb-org
 
 # Start MongoDB service
 sudo service mongod start
